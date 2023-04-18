a40d1c9d7ff5f699b1a9c0b387e40bb45ef0ff52
klh6157 <Scytrius@gmail.com>
2023-04-18 13:07:53 -0400

    committing just to commit

--- src/event-badge.js
+++ src/event-badge.js
@@ -5,6 +5,8 @@ const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 class EventBadge extends LitElement {
   static properties = {
     header: { type: String },
+    title: { type: String },
+    image: { type: String },
   }
 
   static styles = css`
@@ -44,29 +46,29 @@ class EventBadge extends LitElement {
   constructor() {
     super();
     this.header = 'HAX the Web: Headless Authoring eXperience';
-    this.title = 'HAX';
+    this.title = 'Media';
+    this.image = 'https://www.bing.com/images/blob?bcid=SDnwGs.Os34FpQ';
   }
 
   render() {
     return html`
-      <main>
+      <main>\
+        <h2>${this.header}</h2>
         <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
         <h1>${this.title}</h1>
 
       </main>
-      <svg width="1150" height="1500" id="remix-this" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1150 1500" data-v-b2c8125e="" data-v-a09c7bce="">
+      <div>
+      <svg width="1150" height="1500" id="outerSVG" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1150 1500" class="img-fluid" style="vertical-align:top;overflow:visible;max-height:70vh;" data-v-b2c8125e data-v-a09c7bce>
         <defs data-v-b2c8125e="" data-v-a09c7bce="">
           <style data-v-b2c8125e="" data-v-a09c7bce="">
-            @font-face {
-              font-family: 'Robo  to';
-              font-style: normal;
-              font-weight: 400;
-              src: local('Roboto'), local('Roboto-Regular'), url(https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxKKTU1Kvnz.woff2) format('woff2');
-            }
-        
+            
+            <image href="${this.image}" x="0" y="0" height="100%" width="100%" />
+
       </style>
       </defs>
       </svg>
+      </div>
     `;
   }
 }
